0786b83cde22fcfcccab9b95f447795059e425dc
MrArdaix <97133804+MrArdaix@users.noreply.github.com>
2023-11-18 12:32:07 -0500

    added js

---
+++ JS.js
@@ -0,0 +1,18 @@
+const select = document.querySelector("select");
+const response = document.querySelector("response");
+
+select.addEventListener("change", setChoice);
+
+function setChoice(){
+    const choice = select.value;
+
+    if (choice === "snowboarding"){
+        response.textContent = 
+        "Snowboarding is harder to learn but easier to master. Excellent choice!"
+    } else if (choice == "skiing"){
+        response.textContent =
+        "Skiing is easier to learn but harder to master. Wise choice!"
+    } else{
+        response.textContent ="";
+    }
+}
